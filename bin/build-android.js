1a9c166114f850d14e740b7ab185ff1e4db9fd65
James Johnson <jamesjohnson@Jamess-MacBook-Pro.local>
2018-07-01 16:05:14 +0100

    Add build android cli

---
+++ bin/build-android.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+
+const gradle = require("../lib/gradle");
+const files = require('../lib/files');
+
+clear();
+
+// Say hello
+console.log(
+  chalk.yellow(
+    figlet.textSync('Vue Morph', { horizontalLayout: 'full' })
+  )
+);
+
+// Check for existing Android app
+if (!files.directoryExists('apps/android')) {
+  console.log(chalk.red('Can\' find an Android App! You need to add an Android app to the Project.'));
+  console.log('Try running `yarn create-app:android`');
+  process.exit();
+}
+
+const run = async () => {
+  await gradle.assembleDebug();
+};
+
+run();
